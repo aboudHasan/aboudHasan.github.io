22dfb905b151fbf2253b33de2970f245967ff488
aboudHasan <aboudhasan06@gmail.com>
2025-03-25 06:56:19 -0400

    almost done

--- myscript3.js
+++ myscript3.js
@@ -1,135 +1,441 @@
-window.addEventListener("load", () => {
-  let heroes = [
-    {
-      name: "Ana",
-      emoji1: "🎯",
-      emoji2: "💉",
-      emoji3: "👵",
-      emoji4: "😴",
-    },
-    {
-      name: "Ashe",
-      emoji1: "🎯",
-      emoji2: "🤠",
-      emoji3: "🧨",
-    },
-    {
-      name: "Baptiste",
-      emoji1: "🩹",
-      emoji2: "🔫",
-      emoji3: "🙍🏿‍♂️",
-    },
-    {
-      name: "Bastion",
-      emoji1: "🤖",
-      emoji2: "🐤",
-      emoji2: "💣",
-      emoji3: "🔫",
-    },
-    {
-      name: "Brigitte",
-      emoji1: "🐱",
-      emoji2: "🛡",
-      emoji3: "🩹",
-    },
-    {
-      name: "Cassidy",
-      emoji1: "🤠",
-      emoji2: "🚬",
-      emoji3: "🔫",
-      emoji4: "🦾",
-    },
-    {
-      name: "D.Va",
-      emoji1: "🎮",
-      emoji2: "🚀",
-      emoji3: "🐰",
-    },
-    {
-      name: "Doomfist",
-      emoji1: "🥋",
-      emoji2: "🦾",
-      emoji3: "👊🏿",
-    },
-    {
-      name: "Echo",
-      emoji1: "🤖",
-      emoji2: "👩🏻‍⚕️",
-      emoji3: "🔃",
-      emoji4: "👩🏻",
-    },
-    {
-      name: "Genji",
-      emoji1: "🐱‍👤",
-      emoji2: "⭐",
-      emoji3: "🗡️",
-    },
-    {
-      name: "Hanzo",
-      emoji1: "🍡",
-      emoji2: "🍣",
-      emoji3: "🏹",
-      emoji4: "🏯",
-    },
-    {
-      name: "Hazard",
-      emoji1: "🦾",
-      emoji2: "⛓️",
-    },
-    {
-      name: "Illari",
-      emoji1: "🩹",
-      emoji2: "☀️",
-    },
-    {
-      name: "Junker Queen",
-      emoji1: "🗑️",
-      emoji2: "🔪",
-      emoji3: "👑",
-    },
-    {
-      name: "Junkrat",
-      emoji1: "🗑️",
-      emoji2: "💣",
-      emoji3: "🐀",
-    },
-    {
-      name: "Juno",
-      emoji1: "🩹",
-      emoji2: "☄️",
-      emoji3: "🪐",
-      emoji4: "😰",
-    },
-    {
-      name: "Kiriko",
-      emoji1: "🩹",
-      emoji2: "🏯",
-      emoji3: "🦊",
-    },
-    {
-      name: "Lifeweaver",
-      emoji1: "🩹",
-      emoji2: "🌲",
-      emoji3: "🌸",
-    },
-    {
-      name: "Lucio",
-      emoji1: "🩹",
-      emoji2: "🎧",
-      emoji3: "🐸",
-    },
-    {
-      name: "Mauga",
-      emoji1: "🔫",
-      emoji2: "🔫",
-      emoji3: "🔥",
-      emoji4: "💪🏽",
-    },
-    {
-      name: "Mei",
-      emoji1: "🐼",
-      emoji2: "❄",
-      emoji3: "🧊",
-    },
-  ];
+let heroes = [
+  {
+    name: "Ana",
+    emoji1: "🎯",
+    emoji2: "👵",
+    emoji3: "💉",
+    emoji4: "😴",
+    img: "images/Ana.png",
+  },
+  {
+    name: "Ashe",
+    emoji1: "🎯",
+    emoji2: "🤠",
+    emoji3: "🧨",
+    img: "images/Ashe.png",
+  },
+  {
+    name: "Baptiste",
+    emoji1: "🩹",
+    emoji2: "🔫",
+    emoji3: "🙍🏿‍♂️",
+    img: "images/Baptiste.png",
+  },
+  {
+    name: "Bastion",
+    emoji1: "🤖",
+    emoji2: "💣",
+    emoji3: "🐤",
+    emoji4: "🔫",
+    img: "images/Bastion.png",
+  },
+  {
+    name: "Brigitte",
+    emoji1: "🐱",
+    emoji2: "🛡",
+    emoji3: "🩹",
+    img: "images/Brigitte.png",
+  },
+  {
+    name: "Cassidy",
+    emoji1: "🤠",
+    emoji2: "🚬",
+    emoji3: "🔫",
+    emoji4: "🦾",
+    img: "images/Cassidy.png",
+  },
+  {
+    name: "D.Va",
+    emoji1: "🎮",
+    emoji2: "🚀",
+    emoji3: "🐰",
+    img: "images/D.Va.png",
+  },
+  {
+    name: "Doomfist",
+    emoji1: "🥋",
+    emoji2: "🦾",
+    emoji3: "👊🏿",
+    img: "images/Doomfist.png",
+  },
+  {
+    name: "Echo",
+    emoji1: "🤖",
+    emoji2: "👩🏻‍⚕️",
+    emoji3: "🔃",
+    emoji4: "👩🏻",
+    img: "images/Echo.png",
+  },
+  {
+    name: "Genji",
+    emoji1: "⭐",
+    emoji2: "🐱‍👤",
+    emoji3: "🗡️",
+    img: "images/Genji.png",
+  },
+  {
+    name: "Hanzo",
+    emoji1: "🍡",
+    emoji2: "🍣",
+    emoji3: "🏹",
+    emoji4: "🏯",
+    img: "images/Hanzo.png",
+  },
+  {
+    name: "Hazard",
+    emoji1: "🦾",
+    emoji2: "⛓️",
+    emoji3: "🟣",
+    img: "images/Hazard.png",
+  },
+  {
+    name: "Illari",
+    emoji1: "🩹",
+    emoji2: "☀️",
+    img: "images/Illari.png",
+  },
+  {
+    name: "Junker Queen",
+    emoji1: "🗑️",
+    emoji2: "🔪",
+    emoji3: "👑",
+    img: "images/Junker_Queen.png",
+  },
+  {
+    name: "Junkrat",
+    emoji1: "🗑️",
+    emoji2: "💣",
+    emoji3: "🐀",
+    img: "images/Junkrat.png",
+  },
+  {
+    name: "Juno",
+    emoji1: "🩹",
+    emoji2: "☄️",
+    emoji3: "🪐",
+    emoji4: "😰",
+    img: "images/Juno.png",
+  },
+  {
+    name: "Kiriko",
+    emoji1: "🩹",
+    emoji2: "🏯",
+    emoji3: "🦊",
+    img: "images/Kiriko.png",
+  },
+  {
+    name: "Lifeweaver",
+    emoji1: "🩹",
+    emoji2: "🌲",
+    emoji3: "🌸",
+    img: "images/Lifeweaver.png",
+  },
+  {
+    name: "Lucio",
+    emoji1: "🩹",
+    emoji2: "🎧",
+    emoji3: "🐸",
+    img: "images/Lucio.png",
+  },
+  {
+    name: "Mauga",
+    emoji1: "🔫",
+    emoji2: "🔫",
+    emoji3: "🔥",
+    emoji4: "💪🏽",
+    img: "images/Mauga.png",
+  },
+  {
+    name: "Mei",
+    emoji1: "🐼",
+    emoji2: "❄️",
+    emoji3: "🧊",
+    img: "images/Mei.png",
+  },
+  {
+    name: "Mercy",
+    emoji1: "🩹",
+    emoji2: "😇",
+    emoji3: "👩🏼‍⚕️",
+    img: "images/Mercy.png",
+  },
+  {
+    name: "Moira",
+    emoji1: "🧪",
+    emoji2: "🟣",
+    emoji3: "🟡",
+    img: "images/Moira.png",
+  },
+  {
+    name: "Orisa",
+    emoji1: "🤖",
+    emoji2: "👧🏿",
+    emoji3: "🐴",
+    img: "images/Orisa.png",
+  },
+  {
+    name: "Pharah",
+    emoji1: "🚀",
+    emoji2: "🦅",
+    emoji3: "💣",
+    img: "images/Pharah.png",
+  },
+  {
+    name: "Ramattra",
+    emoji1: "🤖",
+    emoji2: "🛡",
+    emoji3: "😡",
+    emoji4: "🦾",
+    img: "images/Ramattra.png",
+  },
+  {
+    name: "Reaper",
+    emoji1: "🔫",
+    emoji2: "🔫",
+    emoji3: "💀",
+    img: "images/Reaper.png",
+  },
+  {
+    name: "Reinhardt",
+    emoji1: "🛡",
+    emoji2: "🚀",
+    emoji3: "🔨",
+    img: "images/Reinhardt.png",
+  },
+  {
+    name: "Roadhog",
+    emoji1: "😷",
+    emoji2: "⛓️",
+    emoji3: "🐷",
+    img: "images/Roadhog.png",
+  },
+  {
+    name: "Sigma",
+    emoji1: "👨‍🦲",
+    emoji2: "⚛️",
+    emoji3: "🎼",
+    emoji4: "☄️",
+    img: "images/Sigma.png",
+  },
+  {
+    name: "Sojourn",
+    emoji1: "🔫",
+    emoji2: "🦾",
+    emoji3: "🦿",
+    emoji4: "👩🏿",
+    img: "images/Sojourn.png",
+  },
+  {
+    name: "Soldier 76",
+    emoji1: "🔫",
+    emoji2: "🎯",
+    emoji3: "👴🏼",
+    img: "images/Soldier76.png",
+  },
+  {
+    name: "Symmetra",
+    emoji1: "🦾",
+    emoji2: "🟦",
+    emoji3: "👩🏾",
+    img: "images/Symmetra.png",
+  },
+  {
+    name: "Torbjorn",
+    emoji1: "🦾",
+    emoji2: "🔨",
+    emoji3: "👨‍👧",
+    img: "images/Torbjorn.png",
+  },
+  {
+    name: "Tracer",
+    emoji1: "🔫",
+    emoji2: "🔫",
+    emoji2: "💨",
+    emoji3: "⏪",
+    img: "images/Tracer.png",
+  },
+  {
+    name: "Widowmaker",
+    emoji1: "🔫",
+    emoji2: "🎯",
+    emoji3: "🟣",
+    emoji4: "🕷️",
+    img: "images/Widowmaker.png",
+  },
+  {
+    name: "Winston",
+    emoji1: "⚡",
+    emoji2: "🌑",
+    emoji3: "🥜",
+    emoji4: "🦍",
+    img: "images/Winston.png",
+  },
+  {
+    name: "Wrecking Ball",
+    emoji1: "⚽",
+    emoji2: "🌑",
+    emoji3: "🐹",
+    img: "images/Wrecking_Ball.png",
+  },
+  {
+    name: "Zarya",
+    emoji1: "💪🏻",
+    emoji2: "🔵",
+    img: "images/Zarya.png",
+  },
+  {
+    name: "Zenyatta",
+    emoji1: "🤖",
+    emoji2: "👁️",
+    emoji3: "🧘‍♂️",
+    img: "images/Zenyatta.png",
+  },
+];
+
+let selectedHero = pickRandomHero();
+let count = 0;
+let traitTest = 0;
+let test = 0;
+let emojiDisplay = document.getElementById("emoji-container");
+
+emojiDisplay.innerHTML += `${selectedHero.emoji1}`;
+
+console.log(selectedHero);
+
+const dropdown = document.getElementById("dropdown");
+const input = document.getElementById("guess");
+
+let filteredHeroes = [];
+let unavailableHeroes = [];
+
+input.addEventListener("input", () => {
+  const query = input.value.toLowerCase();
+  dropdown.innerHTML = "";
+
+  if (query === "") {
+    dropdown.style.display = "none";
+    dropdown.innerHTML = "";
+    return;
+  }
+
+  filteredHeroes = heroes.filter((hero) =>
+    hero.name.toLowerCase().startsWith(query)
+  );
+
+  if (filteredHeroes.length === 0) {
+    dropdown.innerHTML = "No results found";
+    return;
+  }
+
+  filteredHeroes.forEach((hero) => {
+    const option = document.createElement("div");
+    option.innerHTML = `<img src ="${hero.img}"> ${hero.name}`;
+
+    option.addEventListener("click", () => {
+      document.getElementById("guess").value = hero.name;
+      checkGuess();
+      dropdown.style.display = "none";
+      dropdown.innerHTML = "";
+    });
+
+    dropdown.appendChild(option);
+  });
+
+  dropdown.style.display = "block";
 });
+
+// Function to pick a random hero from the list
+function pickRandomHero() {
+  return heroes[Math.floor(Math.random() * heroes.length)];
+}
+
+// Check the user's guess
+function checkGuess() {
+  count++;
+
+  const guessInput = document.getElementById("guess").value.trim();
+  const guessHistoryContainer = document.getElementById("guess-history");
+  const guessedHero = heroes.find(
+    (hero) => hero.name.toLowerCase() === guessInput.toLowerCase()
+  );
+
+  let guessEntry = document.createElement("div");
+  guessEntry.classList.add("guess-entry");
+
+  if (!guessedHero) {
+    guessEntry.textContent = `Invalid hero name - Please try again`;
+    count--;
+  } else {
+    if (!(guessedHero.name === selectedHero.name)) {
+      if (test === 0) {
+        if (selectedHero.emoji2) {
+          emojiDisplay.innerHTML += `${selectedHero.emoji2}`;
+        }
+      } else if (test === 1) {
+        if (selectedHero.emoji3) {
+          emojiDisplay.innerHTML += `${selectedHero.emoji3}`;
+        }
+      } else if (test === 2) {
+        if (selectedHero.emoji4) {
+          emojiDisplay.innerHTML += `${selectedHero.emoji4}`;
+        }
+      }
+      guessEntry.style.backgroundColor = "darkred";
+    }
+
+    // Append hero image
+    const heroImage = document.createElement("img");
+    heroImage.src = guessedHero.img;
+    heroImage.alt = guessedHero.name;
+    heroImage.classList.add("guess-image");
+    heroImage.style.float = "left";
+    heroImage.style.alignSelf = "left";
+    guessEntry.prepend(heroImage);
+    guessEntry.append(guessedHero.name);
+  }
+
+  guessHistoryContainer.appendChild(guessEntry);
+  document.getElementById("guess").value = "";
+  test++;
+
+  heroes = heroes.filter((item) => {
+    if (item === guessedHero) {
+      unavailableHeroes.push(item);
+      return false;
+    }
+    return true;
+  });
+
+  traitTest = 0;
+}
+
+// Play again function to reset the game
+function playAgain() {
+  selectedHero = pickRandomHero();
+  console.log(selectedHero);
+  document.getElementById("guess-history").innerHTML = "";
+  document.getElementById("guess").placeholder = "Enter your guess here";
+  document.getElementById("guess").removeAttribute("disabled");
+  count = 0;
+  test = 0;
+  unavailableHeroes = unavailableHeroes.filter((item) => {
+    heroes.push(item);
+    return false;
+  });
+
+  heroes.sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return +1;
+  });
+}
+
+function handleKeyDown(event) {
+  if (event.key === "Enter") {
+    if (filteredHeroes.length > 0) {
+      document.getElementById("guess").value = filteredHeroes[0].name;
+    }
+    checkGuess();
+    dropdown.style.display = "none";
+    dropdown.innerHTML = "";
+  }
+}
